62d15c8edd8e40527f2942cf8c3f6fe40f2d9fc9
DELL <gavhanekshitij8@gmail.com>
2022-01-06 21:52:26 +0530

    files

--- app/src/main/java/com/example/eventribe/MainActivity.kt
+++ app/src/main/java/com/example/eventribe/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.eventribe
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+//import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
         setContentView(R.layout.activity_main)
+//        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+//        supportActionBar?.setCustomView(R.layout.activity_main)
     }
 }
